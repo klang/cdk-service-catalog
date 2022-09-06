db92eca6347aab43a4fd324fd278e7df17c2c829
Karsten Lang Pedersen <karstenlang@gmail.com>
2022-09-06 08:00:51 +0200

    upgrade to CDK 2

--- cdk_service_catalog/cdk_service_catalog_stack.py
+++ cdk_service_catalog/cdk_service_catalog_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import core as cdk
+import aws_cdk as cdk
 from aws_cdk.aws_iam import PrincipalPolicyFragment
 from aws_cdk.aws_s3_assets import Asset
 from aws_cdk.aws_s3 import Bucket, CfnBucket
@@ -6,16 +6,11 @@ from aws_cdk.aws_servicecatalog import CloudFormationTemplate, Portfolio, CloudF
 from aws_cdk.aws_servicecatalog import CfnCloudFormationProduct as cfnp
 from aws_cdk.aws_servicecatalog import CfnPortfolioProductAssociation
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
-
+from constructs import Construct
 
 class CdkServiceCatalogStack(cdk.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
     
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,12 +1,12 @@
-from aws_cdk import core as cdk
-from aws_cdk.aws_codepipeline import Pipeline
-from aws_cdk.pipelines import CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep
-from aws_cdk.aws_codecommit import IRepository, Repository
-import aws_cdk.aws_iam as iam 
+import aws_cdk as cdk
+from aws_cdk.pipelines import CodeBuildStep, CodePipeline, CodePipelineSource
+from aws_cdk.aws_codecommit import Repository
+
+from constructs import Construct
 
 class PipelineStack(cdk.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         repository = Repository.from_repository_name(self, "Repository", "cdk-service-catalog")
