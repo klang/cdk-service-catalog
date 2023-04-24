50ae28d8fe9110f3c229f31145a9a2b6a9836598
Karsten Lang Pedersen <karstenlang@gmail.com>
2023-04-24 13:13:24 +0200

    outcomment pipeline code to prevent confusion

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import os
 import aws_cdk as cdk
 
 from cdk_service_catalog.cdk_service_catalog_stack import CdkServiceCatalogStack
-from pipeline.pipeline import PipelineStack
+#from pipeline.pipeline import PipelineStack
 
 app = cdk.App()
-PipelineStack(app, "PipelineStack")
+#PipelineStack(app, "PipelineStack")
 
 CdkServiceCatalogStack(app, "CdkServiceCatalogStack")
 
